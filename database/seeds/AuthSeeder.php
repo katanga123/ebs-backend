ca8188458ea201544e4c3e53323c3c73a9273b59
Gudo Fabi <fabiofficial04@gmail.com>
2020-06-09 14:17:52 +0800

    Add auth seeder

---
+++ database/seeds/AuthSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AuthSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name'     => 'Admin',
+            'email'    => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+        ];
+
+        DB::table('user')->insert($user);
+    }
+}
